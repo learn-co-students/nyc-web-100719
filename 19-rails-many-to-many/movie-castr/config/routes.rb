fe109375afdb5e325267aa3516cddc94edf47d33
kenave <kaverbookh@gmail.com>
2019-11-04 17:17:52 -0500

    implemented a solution for casting movies with multiple roles from one form

--- 19-rails-many-to-many/movie-castr/config/routes.rb
+++ 19-rails-many-to-many/movie-castr/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/movies/:id/cast', to: "movies#new_cast", as: 'cast_movie'
   post '/movies/:id/cast', to: "movies#create_cast"
 
+  get '/movies/:id', to: "movies#show", as: 'show_movie'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
