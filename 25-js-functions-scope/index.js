1ef00920a987eef509012f3a4b1bd363741e5f1c
github username <github email address>
2019-11-18 15:39:51 -0500

    functions scope

---
+++ 25-js-functions-scope/index.js
@@ -0,0 +1,29 @@
+
+// function manicMonday() {
+//     return {}
+// }
+
+// let day = "Monday"
+// manicMonday()
+
+//first class object/citizen
+// let obj = {}
+// [obj, 1, 3]
+
+// console.log(day)
+//Global scope, function scope, block scope
+function multiplyByN(multiplier) {
+    return function (number) {
+        return number * multiplier
+    }
+}
+
+
+// function multiplyBy2(num){
+//     return num * 2
+// }
+
+
+
+//IIFE => Immediately Invoked Function Expression
+// function variableFunc(){do some code stuff}
---
+++ 25-js-functions-scope/rubyComparison.rb
@@ -0,0 +1,14 @@
+def manic_monday(day)
+    puts "just another manic #{day}...:/"
+end
+
+def day
+    "Monday"
+end
+# manic_monday(day)
+object = {name: "steven"}
+array = [1,2,3, day]
+def returnArray(array)
+   puts array
+end
+returnArray(array)
